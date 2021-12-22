e1dce63dbb7ca92cf37c6012043a1120d76e1ff5
Jahon <example@gmail.com>
2021-12-22 11:00:22 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//O'tgan darsniki
+
+// alert('Salom Ruxshonaxon')
+// let a = confirm("Yoshingiz 15 dami??")
+// console.log(a)
+
+
+// let b = prompt("Iltimos ismingizni kiriting:")
+// console.log(b)
+//Bugungi dars
+
+const a = 7
+const b = 5
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
