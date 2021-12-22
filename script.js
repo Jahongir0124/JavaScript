aab1146331f6ac98e1b1ab74711965dd71f94a76
Jahon <example@gmail.com>
2021-12-22 11:10:28 +0500

    Added h2 tag

--- script.js
+++ script.js
@@ -8,10 +8,8 @@
 // let b = prompt("Iltimos ismingizni kiriting:")
 // console.log(b)
 //Bugungi dars
-
-const a = 7
-const b = 5
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
+// let a = 8
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
